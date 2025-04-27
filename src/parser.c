a094258d2d45650c933d63f507a991448ce7ed94
Kata Grigorjeva <k.grygoryeva@email.cz>
2025-04-27 22:12:11 +0200

    syntax check of redirections tokens

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: jgrigorj <jgrigorj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:28:09 by jgrigorj          #+#    #+#             */
-/*   Updated: 2025/04/24 22:00:24 by jgrigorj         ###   ########.fr       */
+/*   Updated: 2025/04/27 22:01:55 by jgrigorj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #include "../libft/libft.h"
 #include "../include/token.h"
 
-int	check_tokens(t_token *tokens);
-
+// t_ast	*new_ast_node(void);
 
 t_ast	*parser(t_token *tokens)
 {
@@ -23,6 +22,16 @@ t_ast	*parser(t_token *tokens)
 
 	if (!check_tokens(tokens))
 		return (NULL);
+	// ast = new_ast_node();
 	return (NULL);
 }
 
+// t_ast	*new_ast_node(t_node_type *type, t_cmd_data *data)
+// {
+// 	t_ast	*new_node;
+
+// 	new_node = malloc(sizeof(t_ast));
+// 	if (!new_node)
+// 		return (error_handler("Failed to allocate new ast node\n"), NULL);
+// 	new_node->
+// }
--- src/token_check.c
+++ src/token_check.c
@@ -6,7 +6,7 @@
 /*   By: jgrigorj <jgrigorj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 22:00:56 by jgrigorj          #+#    #+#             */
-/*   Updated: 2025/04/26 19:51:25 by jgrigorj         ###   ########.fr       */
+/*   Updated: 2025/04/27 22:10:39 by jgrigorj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 #include "../libft/libft.h"
 #include "../include/token.h"
 
-int	check_parentheses(t_token *tokens);
+static int	check_parentheses(t_token *tokens);
+static int	check_redirect(t_token *tokens);
 
 int	check_tokens(t_token *tokens)
 {
-	if (!check_parentheses(tokens) || !check_operator(tokens))
+	if (!check_parentheses(tokens) || !check_operator(tokens) \
+	|| !check_redirect(tokens))
 		return (0);
-// check redirect
 	return (1);
 }
 
-int	check_parentheses(t_token *tokens)
+// check that every '(' has its corresponding ')'
+static int	check_parentheses(t_token *tokens)
 {
 	int	paren_status;
 
@@ -48,4 +50,34 @@ int	check_parentheses(t_token *tokens)
 		return (1);
 	error_handler("Syntax error: unclosed '('\n");
 	return (0);
+}
+
+// redirections should be followed by a word or a subshell, 
+// in the case of HEREDOC, the << should be followed by 
+// EOF sequence (word or a subshell?)
+static int	check_redirect(t_token *tokens)
+{
+	while (tokens)
+	{
+		if (tokens->type >= TOKEN_REDIR_IN && tokens->type <= TOKEN_HEREDOC)
+		{
+			if (tokens->next->type == TOKEN_EOF)
+				return (ft_eprintf("minishell: \
+syntax error near unexpected token `newline'\n"), 0);
+			if (tokens->next->type == TOKEN_RPAREN)
+				return (ft_eprintf("minishell: \
+syntax error near unexpected token `)'\n"), 0);
+			if (tokens->next->type == TOKEN_AND)
+				return (ft_eprintf("minishell: \
+syntax error near unexpected token `&&'\n"), 0);
+			if (tokens->next->type == TOKEN_OR)
+				return (ft_eprintf("minishell: \
+syntax error near unexpected token `||'\n"), 0);
+			if (tokens->next->type == TOKEN_PIPE)
+				return (ft_eprintf("minishell: \
+syntax error near unexpected token `|'\n"), 0);
+		}
+		tokens = tokens->next;
+	}
+	return (1);
 }
