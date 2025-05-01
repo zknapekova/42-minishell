7400ef6961e8afa0bad88b786aa3f104e2a4d904
Zuzana Knapekova <zknapeko@akamai.com>
2025-05-01 18:21:40 +0200

    fix typo

--- checker.py
+++ checker.py
@@ -15,18 +15,18 @@ if compile_process.returncode != 0:
     sys.exit(1)
 
 for i in range(len(test_cases)):
-    minishell_run = subprocess.run(f"./minishell {test_cases[0]}", shell=True, text=True, capture_output=True)
+    minishell_run = subprocess.run(f"./minishell {test_cases[i]}", shell=True, text=True, capture_output=True)
     minishell_output = minishell_run.stdout.strip()
 
-    cmd = f"export {test_cases[0]} && env"
+    cmd = f"export {test_cases[i]} && env"
     all_env_vars = subprocess.run(cmd, shell=True, text=True, capture_output=True)
 
     if (minishell_output in all_env_vars.stdout.strip()):
-        print(f"✅ Test {test_cases[0]} passed! ✅")
+        print(f"✅ Test {test_cases[i]} passed! ✅")
     else:
-        print(f"❌ Test {test_cases[0]} failed! ❌")
+        print(f"❌ Test {test_cases[i]} failed! ❌")
 
-    valgrind_process = subprocess.run(f"valgrind ./minishell {test_cases[0]}", shell=True, text=True,
+    valgrind_process = subprocess.run(f"valgrind ./minishell {test_cases[i]}", shell=True, text=True,
                                       capture_output=True)
 
     if ("All heap blocks were freed -- no leaks are possible" in valgrind_process.stderr):
