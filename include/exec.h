4db3f5ad8c34dd788d11fc534b388fdf7511e99b
Kata Grigorjeva <k.grygoryeva@email.cz>
2025-05-22 17:15:24 +0200

    added get_argv() and get_redir_target_str() for transforming args into argv and for merging redirection target string

--- include/exec.h
+++ include/exec.h
@@ -2,7 +2,11 @@
 # define EXEC_H
 
 #include "main.h"
-char	**get_arg_list(t_data *data, t_arg *args);
+char	**get_argv(t_data *data, t_arg *args);
+char	*get_arg_str(t_data *data, t_arg **args);
 void	print_ast_argv(t_data *data, t_ast *node, int depth);
+int	get_list_len(t_arg *args);
+void	free_argv(char **argv);
+char	*get_redir_target_str(t_data *data, t_redir_target *target);
 
 #endif
--- src/exec/argv_utils.c
+++ src/exec/argv_utils.c
@@ -1,27 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec_utils.c                                       :+:      :+:    :+:   */
+/*   argv_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jgrigorj <jgrigorj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/19 17:29:44 by jgrigorj          #+#    #+#             */
-/*   Updated: 2025/05/21 20:08:00 by jgrigorj         ###   ########.fr       */
+/*   Created: 2025/05/22 15:55:18 by jgrigorj          #+#    #+#             */
+/*   Updated: 2025/05/22 17:12:50 by jgrigorj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 #include "libft.h"
 #include "env_vars.h"
+#include "exec.h"
 #include "parser_utils.h" // for print_indent()
 #include <stdlib.h> //for NULL
 
-int		get_list_len(t_arg *args);
-char	*get_arg_str(t_data *data, t_arg **args);
-void	free_argv(char **argv);
-char	*get_redir_target_str(t_data *data, t_redir_target *target);
-
-char	**get_arg_list(t_data *data, t_arg *args)
+char	**get_argv(t_data *data, t_arg *args)
 {
 	char	**argv;
 	char	*arg_str;
@@ -64,7 +60,6 @@ char	*get_arg_str(t_data *data, t_arg **args)
 			tmp_str = ft_strdup((*args)->value);
 		if (!tmp_str)
 			return (error_handler("Error getting arg_str"), NULL);
-		ft_printf("%s\n", tmp_str);
 		arg_str = ft_strjoin_ed(arg_str, tmp_str, ft_strlen(tmp_str));
 		free (tmp_str);
 		if (!arg_str)
@@ -72,7 +67,7 @@ char	*get_arg_str(t_data *data, t_arg **args)
 		if ((*args)->word_join == W_SPLIT)
 		{
 			(*args) = (*args)->next;
-			break;
+			break ;
 		}
 		(*args) = (*args)->next;
 	}
@@ -100,10 +95,10 @@ int	get_list_len(t_arg *args)
 void	free_argv(char **argv)
 {
 	int	i;
-	
+
 	i = 0;
-	if  (!argv)
-		return;
+	if (!argv)
+		return ;
 	while (argv[i])
 	{
 		free (argv[i]);
@@ -116,8 +111,9 @@ void	print_ast_argv(t_data *data, t_ast *node, int depth)
 {
 	char	**argv;
 	int		i;
-	char 	*redir_target;
-	
+	char	*redir_target;
+	t_redir	*redir;
+
 	i = 0;
 	if (!node)
 		return ;
@@ -128,64 +124,34 @@ void	print_ast_argv(t_data *data, t_ast *node, int depth)
 	{
 		if (node->cmd_data->args)
 		{
-			argv = get_arg_list(data, node->cmd_data->args);
+			argv = get_argv(data, node->cmd_data->args);
 			if (!argv)
 				return ;
-			print_indent(depth+1);
+			print_indent(depth + 1);
 			ft_printf("**argv: ");
 			while (argv[i])
-			{
-				ft_printf("%s, ", argv[i]);
-				i++;
-			}
+				ft_printf("%s, ", argv[i++]);
 			ft_printf("\n");
 			free_argv(argv);
 		}
-		if  (node->cmd_data->redirs)
+		if (node->cmd_data->redirs)
 		{
-			print_indent(depth+1);
-			ft_printf("**redir: ");
+			print_indent(depth + 1);
+			ft_printf("**redirs:\n");
+			redir = node->cmd_data->redirs;
+			while (redir && redir->target)
+			{
+				redir_target = get_redir_target_str(data, redir->target);
+				if (!redir_target)
+					return ;
+				print_indent(depth + 2);
+				ft_printf("typ: %s, target: %s\n", \
+					redir_type_to_str(redir->type), redir_target);
+				free (redir_target);
+				redir = redir->next;
+			}
 		}
-		while (node->cmd_data->redirs && node->cmd_data->redirs->target)
-		{
-			redir_target = get_redir_target_str(data, node->cmd_data->redirs->target);
-			if (!redir_target)
-				return ;
-			print_indent(depth+2);
-			ft_printf("type %s, target %s\n", redir_type_to_str(node->cmd_data->redirs->type), redir_target);
-			free (redir_target);
-			node->cmd_data->redirs = node->cmd_data->redirs->next;
-		}
-		
-		
 	}
-	// 	print_cmd_data(node->cmd_data, depth + 1);
-	// ft_printf("\n");
 	print_ast_argv(data, node->left, depth + 1);
 	print_ast_argv(data, node->right, depth + 1);
-}
-
-char	*get_redir_target_str(t_data *data, t_redir_target *target)
-{
-	char	*target_str;
-	char	*tmp_str;
-
-	target_str = NULL;
-	while (target->value)
-	{
-		if (get_first_ind(target->value, '$', 0) != -1 \
-		&& target->quote_type != QUOTE_SINGLE)
-			tmp_str = extend_env_value_nf(data, target->value);
-		else
-			tmp_str = ft_strdup(target->value);
-		if (!tmp_str)
-			return (error_handler("Error getting target_str"), NULL);
-		ft_printf("%s\n", tmp_str);
-		target_str = ft_strjoin_ed(target_str, tmp_str, ft_strlen(tmp_str));
-		free (tmp_str);
-		if (!target_str)
-			return (error_handler("Error getting target_str"), NULL);
-		target = target->next;
-	}
-	return (target_str);
 }
